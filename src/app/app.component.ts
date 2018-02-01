4da824c9af44b3c6f92d86d610f79f0becab9750
Abhijit Kar <ak24284@gmail.com>
2018-02-02 01:11:17 +0530

    Lint Issue with, trailing whitespace and Lifecycle interface OnInit resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router, NavigationEnd } from "@angular/router";
 import { GoogleAnalyticsService } from "./shared/";
 
@@ -9,8 +9,7 @@ declare var ga;
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
   fileDetails: Array<any>;
 
   constructor(
@@ -27,30 +26,41 @@ export class AppComponent {
   ngOnInit() {
     this.fileDetails = [
       {
-        url: "https://drive.google.com/open?id=1d-C8JMkPVzucekBHUEUVKbsmDSp2yE5C",
+        url:
+          "https://drive.google.com/open?id=1d-C8JMkPVzucekBHUEUVKbsmDSp2yE5C",
         title: "Padlocks_Freedom",
         type: "btn btn-primary"
-      }, {
-        url: "https://drive.google.com/open?id=1aiX7zrjQzX0FXG9J8ExCGZ4cTHRMvfR2",
+      },
+      {
+        url:
+          "https://drive.google.com/open?id=1aiX7zrjQzX0FXG9J8ExCGZ4cTHRMvfR2",
         title: "Padlocks_Kadi-Tala_6levers",
         type: "btn btn-dark"
-      }, {
-        url: "https://drive.google.com/open?id=1pZPeT5Au_lTpdNJqcrBnoz5SflAygOsB",
+      },
+      {
+        url:
+          "https://drive.google.com/open?id=1pZPeT5Au_lTpdNJqcrBnoz5SflAygOsB",
         title: "Padlocks_Myloc_SmallWonderSeries",
         type: "btn btn-light"
-      }, {
-        url: "https://drive.google.com/open?id=1kH1FivzOcbtOOrThi9-h5CTwuX9gk-sx",
+      },
+      {
+        url:
+          "https://drive.google.com/open?id=1kH1FivzOcbtOOrThi9-h5CTwuX9gk-sx",
         title: "Padlocks_Navtal",
         type: "btn btn-danger"
-      }, {
-        url: "https://drive.google.com/open?id=1MONe2UH_6F1hLuICLM5c2knROBy0B4Tn",
+      },
+      {
+        url:
+          "https://drive.google.com/open?id=1MONe2UH_6F1hLuICLM5c2knROBy0B4Tn",
         title: "Padlocks_SherlockSolidBrass",
         type: ""
-      }, {
-        url: "https://drive.google.com/open?id=1isXSiu_OZNHPISqNS9S7w5IgjAgcov8Y",
+      },
+      {
+        url:
+          "https://drive.google.com/open?id=1isXSiu_OZNHPISqNS9S7w5IgjAgcov8Y",
         title: "Rakshak",
         type: null
       }
-    ]
+    ];
   }
 }
